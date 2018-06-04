f140f45cc4408d1a7b9ccdca2a14848addd08f7e
Igor Soares <igor.larcs@gmail.com>
2018-06-04 19:09:08 -0300

    adding skip_confirmation! step

--- lib/gull.rb
+++ lib/gull.rb
@@ -10,8 +10,9 @@ module Gull
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      # Fix for raccoon's custom validation...
+      # Setting user as active, skipping email confirmation section...
       user.is_active = true if user.has_attribute?(:is_active)    
+      user.skip_confirmation!
     end
   end
 
