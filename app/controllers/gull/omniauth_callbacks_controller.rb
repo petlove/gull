77286774f288666a4133b683851f6d255375f28f
Gustavo Chapim <gustavo@chapim.com>
2019-03-07 13:09:11 -0300

    Fixing extra parameter (#8)

--- app/controllers/gull/omniauth_callbacks_controller.rb
+++ app/controllers/gull/omniauth_callbacks_controller.rb
@@ -3,13 +3,14 @@
 module Gull
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      omni_auth = request.env['omniauth.auth'].except('extra')
+      @user = User.from_omniauth(omni_auth)
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
-        session['devise.google_data'] = request.env['omniauth.auth']
+        session['devise.google_data'] = omni_auth
         redirect_to new_user_registration_url
       end
     end
